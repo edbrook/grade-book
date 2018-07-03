e24e2f473a50c6e236c3922a54b22490cc6779f2
Ed Brook <edbrook@gmail.com>
2018-07-03 10:53:48 +0100

    use better variable names

--- tests/utils.py
+++ tests/utils.py
@@ -5,40 +5,51 @@ import yaml
 from gradebook.models import Base, Course, Module, ModuleComponent
 
 
-def setupDb(source_data):
+def setupDb(data_file):
     engine = create_engine('sqlite:///:memory:', echo=False)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    with open(source_data,'r') as f:
-        doc = yaml.load(f)
-
-    for c in doc:
-        course = Course(name=c['name'])
-        session.add(course)
-        m_order = 1
-        for m in c['modules']:
-            _process_module(m, course, m_order, session)
-            m_order += 1
-        session.commit()
+    _load_from_file(data_file, session)
 
     return session
 
 
-def _process_module(m, course, m_order, session):
-    components = m.pop('components')
-    module = Module(**m, ordering=m_order)
+def _load_from_file(data_file, session):
+    with open(data_file,'r') as f:
+        doc = yaml.load(f)
+
+    for course_data in doc:
+        _process_course(course_data, session)
+
+    session.commit()
+
+
+def _process_course(course_data, session):
+    course = Course(name=course_data['name'])
+    session.add(course)
+    
+    module_order = 1
+    for module_data in course_data['modules']:
+        _process_module(module_data, course, module_order, session)
+        module_order += 1        
+
+
+def _process_module(module_data, course, module_order, session):
+    components = module_data.pop('components')
+    module = Module(**module_data, ordering=module_order)
     course.modules.append(module)
     session.add(module)
-    c_order = 1
-    for co in components:
-        _process_component(co, module, c_order, session)
-        c_order += 1
+
+    course_order = 1
+    for component_data in components:
+        _process_component(component_data, module, course_order, session)
+        course_order += 1
 
 
-def _process_component(co, module, c_order, session):
-        component = ModuleComponent(**co, ordering=c_order)
+def _process_component(component_data, module, course_order, session):
+        component = ModuleComponent(**component_data, ordering=course_order)
         module.components.append(component)
         session.add(component)
